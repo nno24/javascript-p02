61ef0a8e56dc29c310a8b9d46acecdc1abd9c669
nno24 <nicolai.norseng@gmail.com>
2021-08-08 00:27:36 +0000

    Finialized greeting message including summary of the message sent from submit form - included this in the artistpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,23 @@ function sendEmail(event){
     };
     emailjs.send('service_oos6518','template_pvbbqr9',msgForm,'user_kiqzH3B0iil4Aqd5GL188');
     contactForm.reset();
-    contactForm.remove();
     greetingSummary(msgForm.subject,msgForm.message);
 
 }
 
 function greetingSummary(subj,msg) {
     let msgSummary = `
-        <p><i>${subj}</i></p>
-        <br>
-        <p><i>${msg}</i></p>
+        <h2>Success!</h2>
+        <h2>Thank you for reaching out</h2>
+        <p style="text-align: center;">Subject: <i>${subj}</i></p>
+        <p style="text-align: center;"><i>${msg}</i></p>
     `;
         //Add a greeting message
-        let greeting = document.getElementById('contact-heading');
-        greeting.textContent = "Thank you for reaching out!!";
+        let contactDiv = document.getElementById('contact-div');
+        contactDiv.remove();
     
         let summary = document.createElement("div");
+        summary.style.textAlign = "center";
         summary.innerHTML=msgSummary;
         let contactSection = document.getElementById('contact');
         contactSection.appendChild(summary);
